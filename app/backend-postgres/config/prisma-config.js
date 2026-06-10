1bdd7f94f1e9fe9227d14effd2513c4f4ced4039
Bhavik-SSBDigital <136329455+Bhavik-SSBDigital@users.noreply.github.com>
2026-06-10 18:08:25 +0530

    update

--- app/backend-postgres/config/prisma-config.js
+++ app/backend-postgres/config/prisma-config.js
@@ -2,8 +2,20 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis;
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL + "?connection_limit=10&pool_timeout=20",
+      },
+    },
+  });
 
 globalForPrisma.prisma = prisma;
+
+process.on("beforeExit", async () => {
+  await prisma.$disconnect();
+});
 
 export default prisma;
